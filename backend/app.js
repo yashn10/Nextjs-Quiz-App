59f82ee39ede85f0984fec70ed147170e6765bdd
Yash Nishankar <yashnishankar3102001@gmail.com>
2024-10-02 12:45:35 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ require('./db');
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://nextjs-quiz-app-opal.vercel.app'],
     methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT', 'PATCH'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
     credentials: true // Enable credentials
